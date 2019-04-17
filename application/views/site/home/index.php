49d62efdccdca81aafc9a950d12424c8a5780099
vpduy <vd@embrace-it.com>
2019-04-17 15:20:53 +0700

    fix homepage display

--- application/views/site/home/index.php
+++ application/views/site/home/index.php
@@ -4,10 +4,11 @@
 </div>  
   <?php foreach($maxView as $row):?>
     <a href="<?=site_url('san-pham/chi-tiet/'.$row->slug_name.'-'.$row->id)?>" title="<?php echo $row->name?>" class="internal-link">
-    <div class="col-lg-6 col-sm-12 col-md-6">
+    <div class="col-lg-4 col-sm-4 col-md-4">
       <div class="panel panel-default">
-        <div class="panel-body">
-          <img src="<?php echo base_url('upload/product/'.$row->image_link)?>" class="img-responsive" style="width:100%;height:210px;" alt="<?=$row->name?>"></div>
+        <div class="panel-body" style="width:100%;height:210px;overflow:hidden;text-align:center;background-size:cover;background-image:url(<?php echo base_url('upload/product/'.$row->image_link)?>);">
+          <!-- <img src="<?php echo base_url('upload/product/'.$row->image_link)?>" class="img-responsive" style="display:inline-block;max-height:100%;max-width:100%" alt="<?=$row->name?>"> -->
+          </div>
         <div class="panel-footer"><?=$row->name?></div>
       </div>
     </div>
