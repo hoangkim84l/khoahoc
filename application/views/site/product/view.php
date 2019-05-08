e13d3e512837a21ab2d74caa5afaee55fb19c0c6
Datio <dat@eye4talent.com>
2019-05-08 18:01:12 +0700

    Enhance style

--- application/views/site/product/view.php
+++ application/views/site/product/view.php
@@ -1,26 +1,23 @@
 <div class="page-in-name">
-    <a href="<?php echo base_url()?>" >Trang chủ</a>
-    <a href="<?php echo base_url()?>san-pham.html">Sản phẩm</a>
-    <a href="javascript:void(0)"><?php echo $product->name?></a>
-
+    <a href="<?php echo base_url()?>" ><span>Trang chủ<span></a>
+    <a href="<?php echo base_url()?>san-pham.html"><span>Sản phẩm<span></a>
+    <a ><span><?php echo $product->name?></span></a>
 </div>
+
 <div id="product-view-container">
-    <div class="product-list-part custom-view-list-part">
-        <div class="list-product-container">
+    <div class="single-product-container">
+        <div class="single-product-wrapper">
+            <div class="img-container img-container-view" data-preload-image-url="<?php echo base_url('upload/product/'.$product->image_link)?>">
+            </div>
+            <div class="info product-info">
+                <h1 class="product-title"><?php echo $product->name?></h1>
+                <p class="product-content">
+                    <?php echo $product->content?>
+                </p>
+            </div>    
+        </div>  
+        <!-- <div class="product-content">
             
-            <div class="list-product-wrapper">
-                <div class="img-container img-container-view" data-preload-image-url="<?php echo base_url('upload/product/'.$product->image_link)?>">
-                </div>
-                <div class="info product-info">
-                    <h1 class="product-title"><?php echo $product->name?></h1>
-                    <p class="product-content">
-                        <?php echo $product->content?>
-                    </p>
-                </div>    
-            </div>  
-            <!-- <div class="product-content">
-                
-            </div>   -->
-        </div>
+        </div>   -->
     </div>
 </div>    
